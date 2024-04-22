3f6820b999fed360d085f6dd9a3c8f3de106ec6e
Brett <127159734+sourdoughbredd@users.noreply.github.com>
2024-04-22 10:18:35 -0400

    Add error handling

--- app.js
+++ app.js
@@ -23,4 +23,30 @@ app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/posts", postsRouter);
 
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  res.status(404).json({
+    error: {
+      code: 404,
+      message: "Not Found",
+      detail: "The requested resource was not found on this server.",
+    },
+  });
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // Uncaught error. Send general server error.
+  const statusCode = err.status || 500; // Default to 500 if no specific status is set
+  console.error(err); // Log the error for server-side diagnostics
+
+  res.status(statusCode).json({
+    error: {
+      code: statusCode,
+      message: err.message || "Internal Server Error", // Use a generic message for 500 errors
+      stack: process.env.NODE_ENV === "development" ? err.stack : undefined, // Optionally include the stack trace in development mode
+    },
+  });
+});
+
 module.exports = app;
