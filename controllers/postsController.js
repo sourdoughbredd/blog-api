a0d2538bb26917c21c0fc062ed1799eb5783e0f8
Brett <127159734+sourdoughbredd@users.noreply.github.com>
2024-04-23 17:38:21 -0400

    Implement get all posts func

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,8 +1,11 @@
+const Post = require("../models/post");
 const authorizer = require("../middleware/authorization");
+const asyncHandler = require("express-async-handler");
 
-exports.getAllPosts = (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Get all posts");
-};
+exports.getAllPosts = asyncHandler(async (req, res, next) => {
+  const posts = await Post.find({}).exec();
+  res.status(200).json({ message: "Success", posts });
+});
 
 exports.createPost = [
   authorizer.canCreatePost,
