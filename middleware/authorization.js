d4becb08c5f70429eca0fe678a02e7c6ac7bac39
Brett <127159734+sourdoughbredd@users.noreply.github.com>
2024-04-23 11:48:05 -0400

    Add auth funcs for user CRUD

--- middleware/authorization.js
+++ middleware/authorization.js
@@ -14,3 +14,27 @@ exports.isAuthor = (req, res, next) => {
   }
   next();
 };
+
+exports.canUpdateUser = (req, res, next) => {
+  if (req.params.userId !== req.user._id) {
+    return res.status(401).json({
+      error: {
+        code: 401,
+        message: "Unauthorized: Cannot update other users' info",
+      },
+    });
+  }
+  next();
+};
+
+exports.canDeleteUser = (req, res, next) => {
+  if (req.params.userId !== req.user._id && !req.user.isAuthor) {
+    return res.status(401).json({
+      error: {
+        code: 401,
+        message: "Unauthorized: Cannot delete other users",
+      },
+    });
+  }
+  next();
+};
