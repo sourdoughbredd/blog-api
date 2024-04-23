db720998c91d3a03d526430be5b54a50b7def3f9
Brett <127159734+sourdoughbredd@users.noreply.github.com>
2024-04-23 17:19:08 -0400

    Add authorizer middleware to routes

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,19 +1,30 @@
+const authorizer = require("../middleware/authorization");
+
 exports.getAllPosts = (req, res, next) => {
   res.send("NOT IMPLEMENTED: Get all posts");
 };
 
-exports.createPost = (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Create Post");
-};
+exports.createPost = [
+  authorizer.canCreatePost,
+  (req, res, next) => {
+    res.send("NOT IMPLEMENTED: Create Post");
+  },
+];
 
 exports.getPost = (req, res, next) => {
   res.send(`NOT IMPLEMENTED: Get post ${req.params.postId}`);
 };
 
-exports.updatePost = (req, res, next) => {
-  res.send(`NOT IMPLEMENTED: Update post ${req.params.postId}`);
-};
+exports.updatePost = [
+  authorizer.canUpdatePost,
+  (req, res, next) => {
+    res.send(`NOT IMPLEMENTED: Update post ${req.params.postId}`);
+  },
+];
 
-exports.deletePost = (req, res, next) => {
-  res.send(`NOT IMPLEMENTED: Delete post ${req.params.postId}`);
-};
+exports.deletePost = [
+  authorizer.canDeletePost,
+  (req, res, next) => {
+    res.send(`NOT IMPLEMENTED: Delete post ${req.params.postId}`);
+  },
+];
