c9868bab184e4c560e9f482ea5e85d9e1b8ed056
Brett <127159734+sourdoughbredd@users.noreply.github.com>
2024-04-24 08:59:46 -0400

    Implement create post func

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,6 +1,8 @@
 const Post = require("../models/post");
 const authorizer = require("../middleware/authorization");
+const validator = require("../middleware/validation");
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
 
 exports.getAllPosts = asyncHandler(async (req, res, next) => {
   const posts = await Post.find({}).exec();
@@ -9,9 +11,35 @@ exports.getAllPosts = asyncHandler(async (req, res, next) => {
 
 exports.createPost = [
   authorizer.canCreatePost,
-  (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Create Post");
-  },
+  ...validator.createPostValidator(),
+  asyncHandler(async (req, res, next) => {
+    // Check validation
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        error: {
+          code: 400,
+          message: "Request validation failed",
+          details: errors.array().map((err) => ({
+            field: err.path,
+            error: err.msg,
+          })),
+        },
+      });
+    }
+
+    // All checks passed. Create the post
+    const { title, text, isPublished } = req.body;
+    const post = new Post({
+      user: req.user._id,
+      title,
+      text,
+      timestamp: Date.now(),
+      isPublished,
+    });
+    await post.save();
+    res.status(201).json({ message: "Post created successfully." });
+  }),
 ];
 
 exports.getPost = (req, res, next) => {
