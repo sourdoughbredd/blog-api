703e27e8b8d5d6c80eb13d6a9397e154bda19dda
Brett <127159734+sourdoughbredd@users.noreply.github.com>
2024-04-24 10:12:36 -0400

    Allow only author to get unpublished posts

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,12 +1,23 @@
 const Post = require("../models/post");
 const authorizer = require("../middleware/authorization");
+const { authenticate } = require("../middleware/authentication");
 const validator = require("../middleware/validation");
 const asyncHandler = require("express-async-handler");
+const mongoose = require("mongoose");
 
-exports.getAllPosts = asyncHandler(async (req, res, next) => {
-  const posts = await Post.find({}).exec();
-  res.status(200).json({ message: "Success", posts });
-});
+exports.getAllPosts = [
+  authenticate,
+  asyncHandler(async (req, res, next) => {
+    let query;
+    if (req.user && req.user.isAuthor) {
+      query = {};
+    } else {
+      query = { isPublished: true };
+    }
+    const posts = await Post.find(query).exec();
+    res.status(200).json({ message: "Success", posts });
+  }),
+];
 
 exports.createPost = [
   authorizer.canCreatePost,
@@ -27,9 +38,32 @@ exports.createPost = [
   }),
 ];
 
-exports.getPost = (req, res, next) => {
-  res.send(`NOT IMPLEMENTED: Get post ${req.params.postId}`);
-};
+exports.getPost = asyncHandler(async (req, res, next) => {
+  // Make sure ID provided is valid
+  if (!mongoose.Types.ObjectId.isValid(req.params.postId)) {
+    return res.status(400).json({
+      error: {
+        code: 400,
+        message: "Invalid post ID",
+        id: req.params.postId,
+      },
+    });
+  }
+
+  // Get post
+  const post = await Post.findById(req.params.postId)
+    .populate("user", "username")
+    .exec();
+  if (!post) {
+    return res.status(404).json({
+      error: {
+        code: 404,
+        message: "Could not find post",
+      },
+    });
+  }
+  res.status(200).json({ message: "Success", post });
+});
 
 exports.updatePost = [
   authorizer.canUpdatePost,
