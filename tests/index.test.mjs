6a0ad95e57124c6283846fa759fa5d810faa62e6
Brett <127159734+sourdoughbredd@users.noreply.github.com>
2024-05-14 09:28:10 -0400

    Write tests for index route

---
+++ tests/index.test.mjs
@@ -0,0 +1,28 @@
+// Import dependencies
+import request from "supertest";
+import { expect as _expect } from "chai";
+const expect = _expect;
+import app from "./config/testApp.mjs";
+
+describe("GET index route", function () {
+  it("should return a 200 OK status", async () => {
+    const response = await request(app).get("/");
+    expect(response.status).to.equal(200);
+  });
+
+  it("should return content type as JSON", async () => {
+    const response = await request(app).get("/");
+    expect(response.headers["content-type"]).to.include("application/json");
+  });
+
+  it("should return an object with the correct structure", async () => {
+    const response = await request(app).get("/");
+    expect(response.body).to.include.keys(
+      "message",
+      "version",
+      "documentation_url",
+      "endpoints"
+    );
+    expect(response.body.endpoints).to.have.all.keys("posts", "users");
+  });
+});
