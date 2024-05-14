729bc81824d04c207d93145934c668562fbc9d9b
Brett <127159734+sourdoughbredd@users.noreply.github.com>
2024-05-14 09:19:49 -0400

    Fix http status send bug

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.send(200).json({
+  res.status(200).json({
     message: "Welcome to the Blog API!",
     version: "1.0",
     documentation_url: "https://github.com/sourdoughbredd/blog-api",
