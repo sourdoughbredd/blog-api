4a9dc4985b1701cf11309b9d6f4518ba2627b9a4
Brett <127159734+sourdoughbredd@users.noreply.github.com>
2024-05-14 09:28:56 -0400

    Create testing version of app.js

---
+++ tests/config/testApp.mjs
@@ -0,0 +1,57 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
+import path from "path";
+import cors from "cors";
+import corsOptions from "../../config/cors.js";
+
+import indexRouter from "../../routes/index.js";
+import usersRouter from "../../routes/users.js";
+import postsRouter from "../../routes/posts.js";
+
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+app.use(cors(corsOptions));
+
+// Connect to DB
+// TO DO
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/posts", postsRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  res.status(404).json({
+    error: {
+      code: 404,
+      message: "Not Found",
+      detail: "The requested resource was not found on this server.",
+    },
+  });
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // Uncaught error. Send general server error.
+  const statusCode = err.status || 500; // Default to 500 if no specific status is set
+  console.error(err); // Log the error for server-side diagnostics
+
+  res.status(statusCode).json({
+    error: {
+      code: statusCode,
+      message: err.message || "Internal Server Error", // Use a generic message for 500 errors
+    },
+  });
+});
+
+export default app;
