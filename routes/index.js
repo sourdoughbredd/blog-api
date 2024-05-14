8fc5860ee5b6ad75ab7a7412531e701a9046fb4b
Brett <127159734+sourdoughbredd@users.noreply.github.com>
2024-05-14 08:48:52 -0400

    Return api information from index route

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ const router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+  res.send(200).json({
+    message: "Welcome to the Blog API!",
+    version: "1.0",
+    documentation_url: "https://github.com/sourdoughbredd/blog-api",
+    endpoints: {
+      posts: "/posts",
+      users: "/users",
+    },
+  });
 });
 
 module.exports = router;
